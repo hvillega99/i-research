0f4bd05ac72955e2000af08d38377fb68e179806
hvillega99 <hvillega@espol.edu.ec>
2021-08-02 16:30:24 -0500

    proyecto configurado

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hola mundo")
+const express = require("express");
+const morgan = require("morgan");
+
+//initializations
+const app = express();
+
+//settings
+app.set("port", process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+
+//middlewares
+app.use(morgan("dev"));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//routes
+app.use("/",require("./routes/main"))
+
+//start server
+app.listen(app.get("port"), () => {
+    console.log(`Server listen on localhost:${app.get("port")}`);
+});
