c449d41bbf100f88007c1a6c7634719e21883196
hvillega99 <hvillega@espol.edu.ec>
2021-08-23 13:53:28 -0500

    Clase para hacer peticiones al API de Scopus implementada

--- controllers/investigador.controller.js
+++ controllers/investigador.controller.js
@@ -1,4 +1,15 @@
-exports.getPerfilInvestigador = (req, res) =>{
+const Scopus = require('../helpers/scopus')
+
+
+exports.getPerfilInvestigador = async (req, res) =>{
     const scopusId = req.params.scopusId;
-    res.render("../views/investigador.views.ejs")
+
+    const scopus = new Scopus();
+    const orcidAndCounts = await scopus.getOrcidAndCounts(scopusId);
+    const hIndex = await scopus.getHindex(scopusId);
+
+    const information = {...orcidAndCounts, ...hIndex};
+    information['id'] = scopusId;
+    
+    res.render("../views/investigador.views.ejs", {information: information})
 }
---
+++ helpers/scopus.js
@@ -0,0 +1,40 @@
+const fetch = require('node-fetch');
+const convert = require('xml-js');
+
+class Scopus{
+
+    constructor(){
+        this.uri = "https://api.elsevier.com/content/author/";
+        this.apiKey = "d2f270ed229df1d1aa750351fa2c101b";
+    }
+
+    async getOrcidAndCounts(scopusId){
+        const response = await fetch(`${this.uri}author_id/${scopusId}?apiKey=${this.apiKey}`);
+        const xml = await response.text();
+        const text = convert.xml2json(xml, {compact: true, spaces: 4});
+        const data =  JSON.parse(text)['author-retrieval-response'];
+        return {
+            orcid: data.coredata.orcid._text,
+            documentos: data.coredata['document-count']._text,
+            citas: data.coredata['citation-count']._text,
+        }
+    }
+
+    async getHindex(scopusId){
+        const response = await fetch(`${this.uri}author_id/${scopusId}?apiKey=${this.apiKey}&view=metrics`);
+        const xml = await response.text();
+        const text = convert.xml2json(xml, {compact: true, spaces: 4});
+        const data = JSON.parse(text)['author-retrieval-response'];
+        return{
+            hIndex: data['h-index']._text
+        }
+    }
+
+    async getFCWI(scopusId){
+        
+    }
+
+
+}
+
+module.exports = Scopus;
