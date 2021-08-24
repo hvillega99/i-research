9abf8a4db72b8a1ed0056ba7d0232fae64408dc5
Daniel Viscarra <66076219+danielon20@users.noreply.github.com>
2021-08-24 16:29:17 -0500

    mejora de la funcion busquedaNombre

--- helpers/jsondata.js
+++ helpers/jsondata.js
@@ -4,24 +4,20 @@ class Datajson{
         this.jsondatos = require("../public/data/mySciVal_Researchers_Export.json");
     }
 
-    busquedaNombre(nombre){
-
-        let  data = []
+    busquedaNombre(nombre){    
         let nombres = []
-
-        //const  nombres2= this.jsondatos.filter( resultado =>  resultado["Author"].toLocaleLowerCase().includes(nombre.toLocaleLowerCase()))
-        for(let resultado of this.jsondatos){
-            if(resultado["Author"].toLocaleLowerCase().includes(nombre.toLocaleLowerCase())){
-                if(nombres.indexOf(resultado["Author"])==-1){
-                    nombres.push(resultado["Author"])
-                    data.push(resultado);
+        return this.jsondatos.filter( resultado =>
+            {  
+                if(resultado["Author"].toLocaleLowerCase().includes(nombre.toLocaleLowerCase())){
+                    if(nombres.indexOf(resultado["Author"])==-1){
+                        nombres.push(resultado["Author"])
+                        return true;
+                    }
                 }
+                return false;
             }
-        }
-
-        return data;
+        )
     }
-    
 
     busquedaId(scopuID){
         return this.jsondatos.filter( resultado =>  resultado["Scopus Author ID"]==scopuID )
