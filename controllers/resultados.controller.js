6acc84b16f556cef1768a3dc71356d428dac7bf6
Daniel Viscarra <66076219+danielon20@users.noreply.github.com>
2021-08-24 09:58:20 -0500

    utilizacion de archivo sin autores repetidos

--- controllers/resultados.controller.js
+++ controllers/resultados.controller.js
@@ -4,25 +4,16 @@ exports.find = (req, res) =>{
     console.log('HOla como estan')
     const terms = req.body.terms;
     console.log(terms);
-
-    /*fetch("../public/data/mySciVal_Researchers_Export.json")
-    .then(res => res.json())
-    .then(resultado => {
-        console.log(resultado)
-      }
-    )
-    .catch(console.error);*/
-
-    //const fs = require('fs');
-
-    //let rawdata = fs.readFileSync('../public/data/mySciVal_Researchers_Export.json');
-    //let student = JSON.parse(rawdata);
-    //console.log(student)
-    var resultados = require('../public/data/mySciVal_Researchers_Export.json');
-    //console.log(jaison);
+    //El archivo original
+    //var resultados = require('../public/data/mySciVal_Researchers_Export.json');
+    //El archivo pulido (autores no repetidos)
+    var resultados = require('../public/data/investigadores_data.json');
     let frutas = []
     for(let resultado of resultados){
-        if(resultado["Author"]==terms){
+        //Con el archivo original
+        //if(resultado["Author"]==terms){
+        //Con el archivo pulido (autores no repetidos) //
+        if(resultado["Autor"]==terms){
             frutas.push(resultado);
             console.log(resultado);
         }
