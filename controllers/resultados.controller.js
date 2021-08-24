925175297cd3cc3d0d07116abc0771f4fded7ad8
Daniel Viscarra <66076219+danielon20@users.noreply.github.com>
2021-08-24 00:08:46 -0500

    cambios a la vista y controlador resultados

--- controllers/resultados.controller.js
+++ controllers/resultados.controller.js
@@ -1,10 +1,37 @@
 const Investigadores = require('../models/investigadores.model');
 
 exports.find = (req, res) =>{
+    console.log('HOla como estan')
     const terms = req.body.terms;
-    Investigadores.find({Autor: terms}, (err, docs) => {
+    console.log(terms);
+
+    /*fetch("../public/data/mySciVal_Researchers_Export.json")
+    .then(res => res.json())
+    .then(resultado => {
+        console.log(resultado)
+      }
+    )
+    .catch(console.error);*/
+
+    //const fs = require('fs');
+
+    //let rawdata = fs.readFileSync('../public/data/mySciVal_Researchers_Export.json');
+    //let student = JSON.parse(rawdata);
+    //console.log(student)
+    var resultados = require('../public/data/mySciVal_Researchers_Export.json');
+    //console.log(jaison);
+    let frutas = []
+    for(let resultado of resultados){
+        if(resultado["Author"]==terms){
+            frutas.push(resultado);
+            console.log(resultado);
+        }
+    }
+    res.render("../views/resultados.views.ejs", {total: frutas.length, resultados: frutas, terminos: terms});
+
+    /*Investigadores.find({Autor: terms}, (err, docs) => {
 
         if(err) return res.status(500).send(err.message);
         res.render("../views/resultados.views.ejs", {total: docs.length, resultados: docs, terminos: terms});
-    });
+    });*/
 };
