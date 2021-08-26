7bf57142170696ac9cee2e3ee3a40205ffc21021
hvillega99 <hvillega@espol.edu.ec>
2021-08-26 13:17:56 -0500

    animación de carga mejorada

--- public/js/loading.js
+++
@@ -1,9 +0,0 @@
-items = document.getElementsByClassName("btn-ver-perfil");
-cabecera = document.getElementById("cabecera");
-
-spinner = document.createElement('div');
-spinner.innerHTML = '<div class="spinner-border text-secondary" role="status"><span class="visually-hidden">Loading...</span></div>'
-
-for(let i=0;i<items.length;i++){
-    items[i].onclick = function(event) { cabecera.appendChild(spinner); };
-}
