32d3ac2eed76e0773a9908db99545096fb274ed2
Carlos <carlosqa73@hotmail.com>
2021-11-22 13:30:40 -0500

    [#179513467] Lista de unidades academicas cargadas desde el json

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -8,6 +8,11 @@ exports.getMainPage = async (req, res) => {
     const arrScopusId = dbController.getAllScopusId();
 
     let authors = [];
+    let faculties = [];
+
+    dbController.facultades.forEach(element => {
+        faculties.push(element);
+    });
 
     for(let i=0; i<10; i++){
        let subArray = await scival.getTopH5Index(arrScopusId.slice(Math.round((i/10)*arrScopusId.length), Math.round(((i+1)/10)*arrScopusId.length)));
@@ -17,5 +22,5 @@ exports.getMainPage = async (req, res) => {
     authors.sort((x, y) => y.h5 - x.h5);
     authors = authors.slice(0, 10);
 
-    res.render("../views/main.views.ejs", {authors});
+    res.render("../views/main.views.ejs", {authors, faculties});
 }
