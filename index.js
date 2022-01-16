86bad049f33383db6794ea0f3c6345f46d3aed8c
Daniel Viscarra <dviscarr@espol.edu.ec>
2022-01-16 15:41:45 -0500

    [#180882368] Agregacion del CAS para autenticacion de usuarios admin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path")
 const morgan = require("morgan");
+const passport = require("passport");
 
 //initializations
 const app = express();
@@ -17,6 +18,23 @@ app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+//
+passport.use(new (require('passport-cas').Strategy)({
+    ssoBaseURL: 'http://auth.test.espol.edu.ec',
+    serverBaseURL: 'http://localhost:3000'
+  }, function(login, done) {
+    console.log(login);
+    return done(null, login);
+  }));
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+  
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
 //routes
 app.use('/',require('./routes/main.routes'))
 app.use('/resultados',require('./routes/resultados.routes'));
@@ -25,6 +43,26 @@ app.use('/unidad',require('./routes/unidad_academica.routes'));
 app.use('/centro',require('./routes/centro_investigacion.routes'));
 app.use('/api',require('./routes/api.routes'));
 app.use('/admin', require('./routes/admin.routes'))
+app.use('/cas_login',(req, res, next)=> {
+    passport.authenticate('cas', function (err, user, info) {
+        if (err) {
+          return next(err);
+        }
+        if (!user) {
+          req.session.messages = info.message;
+          return res.redirect('/');
+        }
+        req.logIn(user, function (err) {
+          console.log('XDDDDDDDD');  
+          if (err) {   
+            return next(err);
+          }
+          //req.session.messages = '';
+          return res.redirect('/admin/investigadores');
+        });
+      })(req, res, next);
+})
+
 
 //start server
 app.listen(app.get("port"), () => {
