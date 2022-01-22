ece9856e38ffe79e9ef17f6c8b0c8005f9d20fb1
Daniel Viscarra <dviscarr@espol.edu.ec>
2022-01-21 23:17:47 -0500

    [#180882368] Arreglos en el cierre de sesion del cas

--- index.js
+++ index.js
@@ -28,7 +28,10 @@ app.use(passport.session());
 
 app.use(fileUpload());
 
-
+app.use(function(req, res, next) {
+  res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+  next();
+});
 
 function authenticateA(req,res,next) {
   if(req.isAuthenticated()) {
@@ -88,6 +91,7 @@ app.use('/admin',authenticateA ,require('./routes/admin.routes'));
 app.use('/cas_logout',(req, res)=> {
   req.logout(); // provided by passport
   res.redirect('/');
+  
 });
 
 
