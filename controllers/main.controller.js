ada850198ae6c0c68be4821e0064b34dd464ba08
Daniel Viscarra <dviscarr@espol.edu.ec>
2022-04-06 15:43:07 -0500

    Prueba de algoritmo que muestra publicaciones segun un ODS especifico

--- controllers/main.controller.js
+++ controllers/main.controller.js
@@ -1,10 +1,17 @@
 const Unitsdb = require('../helpers/unitsdb');
 const Centersdb = require('../helpers/centersdb');
+//const Scopus = require('../helpers/scopus'); //Prueba ODS LINEA 1
+//const sdgQueries = require('../helpers/sdgQueries'); //Queris de ODS
 
 const unitsdb = new Unitsdb();
 const centersdb = new Centersdb();
+//const scopus = new Scopus(); // Prueba ODS LINEA 2
 
 exports.getMainPage = async (req, res) => {
+
+    //let infoOFODSP = await scopus.getODSpublications(sdgQueries.sdg1); // Prueba ODS LINEA 3
+    //console.log('INFO de publicaciones de ODS especifico: '); // Prueba ODS LINEA 4
+    //console.log(infoOFODSP);// Prueba ODS LINEA 5
 
     let faculties = [];
     let centros = [];
