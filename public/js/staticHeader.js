6395c5b516333ba77e58e0225e4518210916a48e
Daniel Viscarra <dviscarr@espol.edu.ec>
2022-05-06 21:12:54 -0500

    Mejora en el desplazamiento de elementos en la pagina principal

--- public/js/staticHeader.js
+++ public/js/staticHeader.js
@@ -2,17 +2,22 @@ window.onscroll = function() {myFunction()};
 
 var header = document.getElementById("myHeader");
 var divisionLine = document.getElementById("division");
+var whiteSpace = document.getElementById("white_space");
 var sticky = header.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset > sticky) {
     header.style.position="sticky"
     divisionLine.style.position="sticky"
-    divisionLine.style.top=`${header.offsetHeight}px`
+    whiteSpace.style.position="sticky"
+    whiteSpace.style.height=`${header.offsetHeight}px`
+    divisionLine.style.top=`${header.offsetHeight - 1}px`
+    whiteSpace.style.top=`${header.offsetHeight + 1}px`
   } else {
     header.style.position="relative"
     divisionLine.style.position="relative"
-    divisionLine.style.top="0px"
+    divisionLine.style.top="-1px"
+    whiteSpace.style.height="0px"
   }
 }
 
