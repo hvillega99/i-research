3a3e26c527582908c91ac0e18b53e3e1eaaa7190
Daniel Viscarra <dviscarr@espol.edu.ec>
2022-08-16 17:06:10 -0500

    Temas suegeridos en vista de resultados

--- controllers/resultados.controller.js
+++ controllers/resultados.controller.js
@@ -36,6 +36,35 @@ exports.find = async (req, res) =>{
         })
     }
 
+    //Por aqui debo trabajar con los sinonimos, y/o palabras en ingles/español
+    const the_sinonimos = require('../resources/data/sinonimos.json');
+    const palabras = terms.split(' ');
+
+    /*
+    for(const palabras_s of the_sinonimos){
+        if( palabras_s.includes(palabra)){
+            console.log(palabras_s);
+            break;
+        }
+    }
+    */
+    var sugerencias = [];
+    for (const palabra of palabras){
+        for(const palabras_s of the_sinonimos){
+            for(const the_palabra of palabras_s){
+                if( the_palabra.toUpperCase() === palabra.toUpperCase() ){
+                    sugerencias = sugerencias.concat(palabras_s)
+                }
+            }
+        }
+    }
+
+    const las_sugerencias = [...new Set(sugerencias)]
+    const total_sug = las_sugerencias.length
+    
+    
+
+    
     const kwResults = await gtsi.getAuthorsByKeywords(terms.replace(' ', ';'));
     const idList = Object.keys(kwResults);
     const autByKw = [];
@@ -63,7 +92,9 @@ exports.find = async (req, res) =>{
                                                 centros: resultadosCentros, 
                                                 autoresKw: autByKw,
                                                 totalKw: autByKw.length,
-                                                terminos: terms});
+                                                terminos: terms,
+                                                totalSug: total_sug,
+                                                temas_sugeridos: las_sugerencias});
 };
 
 exports.renderFinder = (req, res) => {
