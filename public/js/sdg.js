6bf03d991231caf534e97523b07f4be95e6bb72a
Daniel Viscarra <dviscarr@espol.edu.ec>
2022-08-22 15:13:28 -0500

    Contenido de publicaciones de ODS 2

--- public/js/sdg.js
+++ public/js/sdg.js
@@ -153,6 +153,7 @@ const showList = async (sdg) =>{
 
     const response2 = await fetch(`/api/ods/projects/${sdg}`);
     const projects = await response2.json();
+    console.log(projects);
     
     const total = document.getElementById(`num-total-projects-${sdg}`);
 
